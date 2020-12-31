6a6c2e2fc30c27bd297609a551042a0e36d22e20
Mohammed Bouri <54592816+BouriMohammed@users.noreply.github.com>
2020-12-31 18:22:09 +0100

    Updates

---
+++ precache-manifest.085b1efdff7980bd4e66f27b45f7f831.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c57dbf467513418f7163b0cea629c648",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "fa25f2455f3ce05cf246",
+    "url": "/home/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "f08f5c2faeba83ba75d8",
+    "url": "/home/static/js/2.3b189aff.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.3b189aff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa25f2455f3ce05cf246",
+    "url": "/home/static/js/main.ef325d85.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "5978a31ea2ce9d69a0aad4a0ea97ed8d",
+    "url": "/home/static/media/photo.5978a31e.jpg"
+  },
+  {
+    "revision": "29a4ef1c570a7e1dee7019b09050aef6",
+    "url": "/home/static/media/resume.29a4ef1c.pdf"
+  }
+]);
